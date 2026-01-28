399409d6cc8c3f1065885cf2dd28df78c0e93818
aymane124 <khaliss_aymane@emsi-edu.ma>
2026-01-28 15:28:29 +0100

    Remove email credentials from settings.py
    Removed sensitive email credentials from settings.

--- firewallbackend/firewallbackend/settings.py
+++ firewallbackend/firewallbackend/settings.py
@@ -350,9 +350,7 @@ EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
 EMAIL_PORT = int(os.getenv('EMAIL_PORT', '587'))
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True').lower() == 'true'
 EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', 'False').lower() == 'true'
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'aymanekhaliiss23@gmail.com')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'jvobgqgyxduxipwz')
-DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'aymanekhaliiss23@gmail.com')
+
 
 # Channels Configuration
 CHANNEL_LAYERS = {
